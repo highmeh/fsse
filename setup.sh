97a05f6052f293a4326893d3a22bcdd78d0974ff
James Hancock <jayme@Jamess-MacBook-Pro.local>
2022-07-19 00:29:13 -0400

    2022 Updates

--- setup.sh
+++ setup.sh
@@ -22,7 +22,7 @@ apt-get update > /dev/null 2>&1
 apt-get upgrade -y > /dev/null 2>&1
 
 echo "[+] Installing tools from apt..."
-apt-get install -qq -y tmux apache2 php libapache2-mod-php vim golang python3-dev git python3 python3-pip > /dev/null 2>&1
+apt-get install -qq -y tmux apache2 php libapache2-mod-php vim golang python3-dev git python3 python3-pip metasploit-framework > /dev/null 2>&1
 
 echo "[+] Setting up GoReport"
 git clone https://github.com/chrismaddalena/Goreport.git /usr/share/Goreport > /dev/null 2>&1
