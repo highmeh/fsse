9dd30c5c6e6f7df8258106123e8f1aac392eafa2
Jayme Hancock <jayme@Jaymes-MacBook-Air.local>
2023-08-06 10:13:54 -0700

    webscreenshot

---
+++ webscreenshot.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+while getopts f: flag
+do
+    case "${flag}" in
+        f) filename=${OPTARG};;
+    esac
+done
+
+while read i; 
+do
+        echo Screenshotting: $i
+        cutycapt --url=https://$i --out=$i.jpg --max-wait=1
+done < $filename
+
